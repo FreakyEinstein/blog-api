edd70d383f461833ed7abae49067e47dea27d5bd
Nanda <nanda@IBEEs-MacBook-Air.local>
2022-10-28 01:14:06 +0530

    structure

---
+++ app/config.py
@@ -0,0 +1,9 @@
+from pydantic import BaseSettings
+from typing import Set
+
+
+class AppConfig(BaseSettings):
+    allowed_origins: Set[str]
+    jwt_secret_key: str
+    mongo_uri: str
+    db_name: str
